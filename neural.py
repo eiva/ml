5def5b5f938fbe7ac6897e42d12466a71bc95aa0
Eugene Ivanchenko <eugene.ivanchenko@outlook.com>
2015-12-19 17:51:33 -0500

    Comments and cleanup

--- neural.py
+++ neural.py
@@ -13,28 +13,29 @@ def dg(g):
 class Network:
     'Primitive activation linear neural network'
     
-    def __init__(self, layer_sizes):
+    def __init__(self, layer_sizes : list):
         'init network specified by give sizes and initialize it by random wights'
-        self.layers=[]
-        self.activations = {}
-        self.delta = {}
-        self.df = {}
+        self.fi=[] # weights of network
+        self.a = {} # output activation from neurons
+        self.delta = {} # delta function
+        self.df = {} # corection for weights
+
         # init by random values
         for i in range(1, len(layer_sizes)):
             random_array = np.random.rand(layer_sizes[i], layer_sizes[i-1] + 1)
-            self.layers.append(np.asmatrix(random_array))
+            self.fi.append(np.asmatrix(random_array))
             
     def result(self):
         'calculated result of forward propogation'
-        return self.activations[len(self.layers)]
+        return self.a[len(self.fi)]
 
     def forward(self, x):
         'calculate the result of forward propogation'
-        self.activations[0] = np.matrix.transpose(x)
-        for l in range(0, len(self.layers)):
-            inp = np.vstack([np.asmatrix([1]), self.activations[l]])
-            z = self.layers[l] * inp
-            self.activations[l+1] = g(z)
+        self.a[0] = np.matrix.transpose(x)
+        for l in range(0, len(self.fi)):
+            inp = np.vstack([np.asmatrix([1]), self.a[l]])
+            z = self.fi[l] * inp
+            self.a[l+1] = g(z)
         return self.result()
         
     def backward(self, y):
@@ -46,28 +47,33 @@ class Network:
             'Distance between elements'
             return ((y - a) ** 2)/2
 
+        # Calculate backpropogation step for output layer
+
         @np.vectorize
         def calcdeltal(y, a):
             'Calculate delta on layer L (output layer)'
             return - (y - a) * dg(a)
 
-        L = len(self.layers)
-        delta = calcdeltal(y, self.activations[L])
+        L = len(self.fi)
+        delta = calcdeltal(y, self.a[L])
         
         self.delta[L] = delta;
 
-        a = np.vstack([[1], self.activations[L-1]])
+        a = np.vstack([[1], self.a[L-1]])
         df = delta * np.matrix.transpose(a)
         self.df[L] = df
-        f = self.layers[L-1]
+        f = self.fi[L-1]
+
+        # Calculate backprop for all hidden layer
+
+        def calcsum(delta, fi):
+            'calculate dE/dA^(l-1)'
+            sum = 0
+            todo
         
         E = distance(y, self.result());
         Etotal = sum(E)
         print(Etotal)
-
-
-        
-        # Backward pass for output layer
         
         
 
