ae75c022bd83c2f21a1601c19783b21f0c7e157a
eiva <eivapub@gmail.com>
2015-12-19 05:55:22 +0000

    Start backprop

--- neural.py
+++ neural.py
@@ -22,6 +22,10 @@ class Network:
         for i in range(1, len(layer_sizes)):
             random_array = np.random.rand(layer_sizes[i], layer_sizes[i-1] + 1)
             self.layers.append(np.asmatrix(random_array))
+            
+    def result(self):
+        'calculated result of forward propogation'
+        return self.activations[len(self.layers)]
 
     def forward(self, x):
         'calculate the result of forward propogation'
@@ -30,12 +34,27 @@ class Network:
             inp = np.vstack([np.asmatrix([1]), self.activations[l]])
             z = self.layers[l] * inp
             self.activations[l+1] = g(z)
-        return self.activations[len(self.layers)]
+        return self.result()
         
     def backward(self, y):
         'make a correction of weights based on expectations for last result'
-        pass
+        
+        @np.vectorize
+        def distance(y, a):
+            'Distance between elements'
+            return ((y - a) ** 2)/2
+        
+        y = np.matrix.transpose(y)
+        E = distance(y, self.result());
+        Etotal = sum(E)
+        print(Etotal)
+        
+        # Backward pass for output layer
+        
+        
 
 net = Network([5, 10, 2])
 
 print(net.forward(np.asmatrix([1,2,3,4,5])))
+
+net.backward(np.asmatrix([0,1]))
